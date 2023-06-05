d01cc2ef32e8156e0852be78268416ce4c153827
Nikita Sherbakov <nikita@sherbakov.dev>
2023-06-05 21:20:54 +0300

    fix: :bug: Fix Edit this page link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,8 +41,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/d3vx-com/brc721-docs/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/d3vx-com/brc721-docs/tree/main/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
